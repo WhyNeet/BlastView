939c400118bab3d339b922b1c911ddd655fe4c89
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-09-14 21:01:31 +0300

    feat(examples): add hello world example

---
+++ examples/hello_world/src/main.rs
@@ -0,0 +1,18 @@
+use blastview::{component::Component, node::Node};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    blastserver::serve(MyComponent).await
+}
+
+struct MyComponent;
+
+impl Component for MyComponent {
+    fn render(&self) -> impl Into<Node> {
+        Node::new("div")
+            .attr("class", "container")
+            .child("Hello world!")
+            .child(Node::new("hr"))
+            .child(Node::new("strong").child("BlastView works"))
+    }
+}
