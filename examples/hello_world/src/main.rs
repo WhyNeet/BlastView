6553f01bf611d8f699d5505cc6023c6a005b10ab
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-09-14 23:00:08 +0300

    refactor(examples): use views

--- examples/hello_world/src/main.rs
+++ examples/hello_world/src/main.rs
@@ -1,18 +1,38 @@
-use blastview::{component::Component, node::Node};
+use blastview::{node::Node, view::View};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    blaster::serve(MyComponent).await
+    blaster::serve(|| MyView).await
 }
 
-struct MyComponent;
+struct MyView;
 
-impl Component for MyComponent {
-    fn render(&self) -> impl Into<Node> {
+impl View for MyView {
+    fn render(&self, cx: &blastview::view::context::ViewContext) -> impl Into<Node> {
+        let counter = cx.create(|| CounterView);
+
         Node::new("div")
             .attr("class", "container")
-            .child("Hello world!")
-            .child(Node::new("hr"))
-            .child(Node::new("strong").child("BlastView works"))
+            .child(my_component())
+            .child(counter)
     }
+}
+
+struct CounterView;
+
+impl View for CounterView {
+    fn render(&self, cx: &blastview::view::context::ViewContext) -> impl Into<Node> {
+        Node::new("button")
+            .on("click", || {
+                println!("increment");
+            })
+            .child(format!("Count: 0"))
+    }
+}
+
+fn my_component() -> impl Into<Node> {
+    Node::new("div")
+        .child("Hello world!")
+        .child(Node::new("hr"))
+        .child(Node::new("strong").child("BlastView works"))
 }
