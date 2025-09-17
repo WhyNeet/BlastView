1182ab3e93f17273041deed77ead50f0865e7c72
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-09-17 13:49:30 +0300

    feat(examples): add html safety example

---
+++ examples/html_escape/src/main.rs
@@ -0,0 +1,47 @@
+use blastview::{node::Node, view::View};
+use tracing_subscriber::{
+    filter::{EnvFilter, LevelFilter},
+    fmt,
+    layer::SubscriberExt,
+    util::SubscriberInitExt,
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env()
+                .unwrap(),
+        )
+        .init();
+
+    blaster::serve(|| MyView).await
+}
+
+struct MyView;
+
+impl View for MyView {
+    fn render(&self, _: &blastview::view::context::ViewContext) -> impl Into<Node> {
+        Node::new("div")
+            .attr("class", "container")
+            .child(my_component())
+    }
+}
+
+fn my_component() -> impl Into<Node> {
+    Node::new("div")
+        .child(r#"<script>alert("oh hey there")</script>"#)
+        .child(
+            Node::new("div")
+                .attr("data-whatever", r#"bad "" attribute ""#)
+                .child("This div has a bad attribute"),
+        )
+        .child(
+            Node::new("div")
+                .child("This one's onclick attribute was removed.")
+                .attr("onclick", r#"alert("hii")"#),
+        )
+}
