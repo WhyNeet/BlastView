b88975b2909fbf883907d3bd2614eb6913e1a016
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-09-17 13:40:35 +0300

    feat(examples): reimplement counter example

--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -1,4 +1,9 @@
-use blastview::{node::Node, view::View};
+use std::{sync::Arc, time::Duration};
+
+use blastview::{
+    node::Node,
+    view::{View, context::ViewRef},
+};
 use tracing_subscriber::{
     filter::{EnvFilter, LevelFilter},
     fmt,
@@ -18,7 +23,28 @@ async fn main() -> std::io::Result<()> {
         )
         .init();
 
-    blaster::serve(|| CounterView).await
+    blaster::serve(|| AppView).await
+}
+
+struct AppView;
+
+impl View for AppView {
+    fn render(&self, cx: &blastview::view::context::ViewContext) -> impl Into<Node> {
+        let counter_view = cx.create(|| CounterView);
+        let auto_counter_view = cx.create(|| AutoIncrementCounterView);
+
+        Node::new("main")
+            .child(view_wrapper("Counter", counter_view))
+            .child(Node::new("hr"))
+            .child(view_wrapper("Auto-increment counter", auto_counter_view))
+    }
+}
+
+fn view_wrapper(view_name: &str, view: ViewRef) -> impl Into<Node> {
+    Node::new("div")
+        .attr("class", "container")
+        .child(Node::new("h3").child(view_name.to_string()))
+        .child(view)
 }
 
 struct CounterView;
@@ -27,16 +53,33 @@ impl View for CounterView {
     fn render(&self, cx: &blastview::view::context::ViewContext) -> impl Into<Node> {
         let (count, set_count) = cx.use_state(0);
 
+        let set_count_decrement = Arc::clone(&set_count);
         Node::new("div")
-            .attr("class", "container")
             .child(
                 Node::new("button")
-                    .child(format!("count: {count}"))
+                    .child(format!("Sub"))
+                    .on("click", move || set_count_decrement(count - 1)),
+            )
+            .child(count.to_string())
+            .child(
+                Node::new("button")
+                    .child(format!("Add"))
                     .on("click", move || set_count(count + 1)),
             )
-            .child(Node::new("p").child(format!(
-                "Count is {}",
-                if count % 2 == 0 { "even" } else { "odd" }
-            )))
+    }
+}
+
+struct AutoIncrementCounterView;
+
+impl View for AutoIncrementCounterView {
+    fn render(&self, cx: &blastview::view::context::ViewContext) -> impl Into<Node> {
+        let (count, set_count) = cx.use_state(0);
+
+        tokio::spawn(async move {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+            set_count(count + 1);
+        });
+
+        Node::new("div").child(format!("Count is: {}", count.to_string()))
     }
 }
