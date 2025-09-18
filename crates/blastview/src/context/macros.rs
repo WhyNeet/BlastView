6ec083f9b2150b58d3b082fb1d69d1dd4e5d0eb3
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-09-18 16:06:14 +0300

    feat(blastview): implement use_effect and use_memo macros

--- crates/blastview/src/context/macros.rs
+++ crates/blastview/src/context/macros.rs
@@ -4,3 +4,34 @@ macro_rules! use_state {
         $cx.use_state($value)
     };
 }
+
+#[macro_export]
+macro_rules! use_effect {
+    ($cx:expr, $callback:expr $(,)?) => {
+      $cx.use_effect($callback, &[] as &[u8])
+    };
+    ($cx:expr, $callback:expr, $( $dep:expr ),+ $(,)?) => {
+      $cx.use_effect($callback, &[$( $dep ),+])
+    };
+}
+
+#[macro_export]
+macro_rules! use_memo {
+  ($cx:expr, $callback:expr $(,)?) => {
+    {
+      let (state, set_state) = $cx.use_state($callback());
+      state
+    }
+  };
+  ($cx:expr, $callback:expr, $( $dep:expr ),+ $(,)?) => {
+    {
+      let (state, set_state) = $cx.use_state($callback());
+
+      $cx.use_effect(move || {
+        set_state($callback());
+      }, &[$( $dep ),+])
+
+      state
+    }
+  };
+}
