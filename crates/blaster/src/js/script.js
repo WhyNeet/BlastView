984c0e26715c9c89d33596e93552199683a71466
git push force <117515380+WhyNeet@users.noreply.github.com>
2025-09-19 18:41:30 +0300

    feat(blaster): implement diffing

--- crates/blaster/src/js/script.js
+++ crates/blaster/src/js/script.js
@@ -4,25 +4,115 @@ ws.addEventListener("open", () => setupEventListeners(document));
 ws.addEventListener("message", (e) => {
   {
     const patch = JSON.parse(e.data);
-    switch (patch["type"]) {
-      case "ReplaceInner":
+    applyPatch(patch);
+  }
+});
+
+function applyPatch(patch) {
+  switch (patch["type"]) {
+    case "ReplaceInner":
+      {
         const elements = document.querySelectorAll(patch["selector"]);
         elements.forEach((element) => {
           cleanupEventListeners(element);
           element.innerHTML = patch["html"];
           setupEventListeners(element);
         });
-        break;
-    }
+      }
+      break;
+    case "ReplaceOuter":
+      {
+        const elements = document.querySelectorAll(patch["selector"]);
+        elements.forEach((element) => {
+          cleanupEventListeners(element);
+          element.outerHTML = patch["html"];
+          setupEventListeners(element);
+        });
+      }
+      break;
+    case "SetAttribute":
+      {
+        const elements = document.querySelectorAll(patch["selector"]);
+        elements.forEach((element) => {
+          element.setAttribute(patch["name"], patch["value"]);
+        });
+      }
+      break;
+    case "RemoveAttribute":
+      {
+        const elements = document.querySelectorAll(patch["selector"]);
+        elements.forEach((element) => {
+          element.removeAttribute(patch["name"]);
+        });
+      }
+      break;
+    case "ReplaceChild":
+      {
+        const elements = document.querySelectorAll(patch["selector"]);
+        elements.forEach((element) => {
+          console.log(element);
+          const oldChild = element.childNodes[patch["index"]];
+          cleanupEventListeners(oldChild);
+          const div = document.createElement("div");
+          div.innerHTML = patch["html"];
+          const newChild = div.firstChild;
+          element.replaceChild(newChild, oldChild);
+          setupEventListeners(newChild);
+        });
+      }
+      break;
+    case "RemoveElement":
+      {
+        const elements = document.querySelectorAll(patch["selector"]);
+        elements.forEach((element) => {
+          cleanupEventListeners(element);
+          element.remove();
+        });
+      }
+      break;
+    case "AttachEvent":
+      {
+        const elements = document.querySelectorAll(patch["selector"]);
+        elements.forEach((element) => {
+          if (element.dataset.events) {
+            element.dataset.events = element.dataset.events + "," + patch["event"];
+          } else {
+            element.dataset.events = patch["event"];
+          }
+          element.addEventListener(patch["event"], handleEvent);
+        });
+      }
+      break;
+    case "DetachEvent":
+      {
+        const elements = document.querySelectorAll(patch["selector"]);
+        elements.forEach((element) => {
+          if (element.dataset.events) {
+            const events = element.dataset.events.split(",");
+            element.dataset.events = events.filter(event => event !== patch["event"]);
+          }
+          element.removeEventListener(patch["event"], handleEvent);
+        });
+      }
+      break;
+    case "Batch":
+      patch["patches"].forEach(applyPatch);
   }
-});
+}
+
 function setupEventListeners(element) {
-  element.querySelectorAll("[data-id]").forEach((element) => {
+  element.querySelectorAll?.("[data-events]")?.forEach((element) => {
     const events = element.dataset.events.split(",");
     events.forEach((eventType) =>
       element.addEventListener(eventType, handleEvent),
     );
   });
+  if (element.dataset?.events) {
+    const events = element.dataset.events.split(",");
+    events.forEach((eventType) =>
+      element.addEventListener(eventType, handleEvent),
+    );
+  }
 }
 
 function handleEvent(e) {
@@ -36,12 +126,18 @@ function handleEvent(e) {
 }
 
 function cleanupEventListeners(element) {
-  element.querySelectorAll("[data-id]").forEach((element) => {
+  element.querySelectorAll?.("[data-events]")?.forEach((element) => {
     const events = element.dataset.events.split(",");
     events.forEach((eventType) =>
       element.removeEventListener(eventType, handleEvent),
     );
   });
+  if (element.dataset?.events) {
+    const events = element.dataset.events.split(",");
+    events.forEach((eventType) =>
+      element.removeEventListener(eventType, handleEvent),
+    );
+  }
 }
 
 class View extends HTMLElement { }
